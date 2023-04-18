15d56335102ee39e6079060c226ef3f03ba92fab
krishna123dakshi143 <poornimakrishna.k@gmail.com>
2023-04-18 13:02:50 +0530

    gitprogram1

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,23 @@
+package org.add;
+
+public class GreensTech {
+	private void greensOmr(String greensadd) {
+		System.out.println("Greens Address:"+greensadd);
+		}
+	private void greensOmr(int greensid,float empsal) {
+		System.out.println("Greens Id:"+greensid);
+		System.out.println("Greens salary:"+empsal);
+		
+		}
+	private void greensOmr(float empsal,int greensid) {
+		System.out.println("Greens Id:"+greensid);
+		System.out.println("Greens salary:"+empsal);
+		}
+public static void main(String[] args) {
+	GreensTech g=new GreensTech();	
+	 g.greensOmr("chennai");
+	 g.greensOmr(3453456,45678.988f);
+	 g.greensOmr(567345.89f,3434565);
+	
+     }	 
+}
